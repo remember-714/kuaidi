7f485cce28b044d07c3f1d4d362461d195cc00db
xuan <947955250@qq.com>
2018-11-13 21:16:48 +0800

    2018/11/13

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # kuaidi
 京东快递小程序
+
+![avatar](/images/1.png)
+![avatar](/images/2.png)
---
+++ pages/index/index.js
@@ -0,0 +1,39 @@
+//index.js
+
+
+Page({
+  data: {
+    value: ''
+  },
+  handleChange: function (event) {
+    let value = event.detail.value;
+    this.setData({ value })
+  },
+  handleSubmit: function () {
+    let value = this.data.value;
+    if (!value) {
+      wx.showToast({
+        icon: 'none',
+        title: '请输入快递号'
+      })
+    } else {
+      let url = `/pages/detail/detail?order_id=${value}`;
+      wx.navigateTo({ url });
+    }
+  },
+  //事件处理函数
+  handleScancode: function () {
+    wx.scanCode({
+      success(res) {
+        let order_id = res.result;
+        order_id = Number(order_id);
+        let url = `/pages/detail/detail?order_id=${order_id}`;
+        wx.navigateTo({ url });
+      }
+    })
+  },
+  onLoad: function () {
+    
+  }
+  
+})
